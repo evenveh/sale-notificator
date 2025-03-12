796bde34989b81898f3b23eb51201ce56724f825
evenveh <even.vehus@gmail.com>
2025-03-12 15:47:52 +0100

    Added functionality to send mail

--- notification.py
+++ notification.py
@@ -1,4 +1,16 @@
+import smtplib
+from Secrets import PASSWORD, TEST_EMAIL, RECIPIENT
 
 
-def send_notification(guitars, reciever_email):
-    print(f'DUMMY: Found {guitars[0]} and {guitars[1]}. Sending notification to {reciever_email}')
+
+def send_mail(guitars):
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=TEST_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=TEST_EMAIL,
+                            to_addrs=RECIPIENT,
+                            msg = f"Subject: New Ibanez at Evenstad outlet\n\nThere are currently {len(guitars)} guitars at Evenstad's outlet page:\n"
+                                  f"{guitars[0]}\n and {guitars[1]}")
+
+    print(f"An email has been sent to {RECIPIENT}")
