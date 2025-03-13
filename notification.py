165f66ae853a5167a9d7212b1eda3efefb16fba8
evenveh <even.vehus@gmail.com>
2025-03-13 08:44:59 +0100

    Unwrapping string so that all guitars are in the mail

--- notification.py
+++ notification.py
@@ -1,16 +1,18 @@
 import smtplib
 from Secrets import PASSWORD, TEST_EMAIL, RECIPIENT
 
-
+def unwrap_guitar_string(guitars):
+    guitars_string = "\n".join(guitars)
+    return guitars_string
 
 def send_mail(guitars):
-
+    guitars_string = unwrap_guitar_string(guitars)
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=TEST_EMAIL, password=PASSWORD)
         connection.sendmail(from_addr=TEST_EMAIL,
                             to_addrs=RECIPIENT,
                             msg = f"Subject: New Ibanez at Evenstad outlet\n\nThere are currently {len(guitars)} guitars at Evenstad's outlet page:\n"
-                                  f"{guitars[0]}\n and {guitars[1]}")
+                                  f"{guitars_string}")
 
     print(f"An email has been sent to {RECIPIENT}")
