f30e9c0c0da586d5a8d161f02c81f11b2a0090c5
evenveh <even.vehus@gmail.com>
2025-03-17 09:38:26 +0100

    Moved urls to secrets folder

--- seleniumtest.py
+++ seleniumtest.py
@@ -3,10 +3,13 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from Secrets import URL_ALL_GUITARS, URL_IBANEX_GUITARS, URL_IBANEZ_GUITARS_AND_BASS
 import time
 
 
+
 def get_guitar_sales():
+    url = URL_IBANEZ_GUITARS_AND_BASS
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -14,16 +17,11 @@ def get_guitar_sales():
     chrome_options.add_argument("--no-sandbox")
 
     service = Service(ChromeDriverManager().install())
+
     with webdriver.Chrome(service=service, options=chrome_options) as driver:
 
-        url_Ibanez_guitars = "https://evenstadmusikk.no/search?q=lagersalg&Filter=ProdusentID%C2%A41:ProdusentID%C2%A41_234%7CPrdGruppeLev2ID%C2%A41:PrdGruppeLev2ID%C2%A41_37%7CPrdGruppeLev1ID%C2%A41:PrdGruppeLev1ID%C2%A41_7"
-        url_Ibanez_guitars_and_bass="https://evenstadmusikk.no/search?q=lagersalg&Filter=ProdusentID%C2%A41:ProdusentID%C2%A41_234|PrdGruppeLev1ID%C2%A41:PrdGruppeLev1ID%C2%A41_7"
-        url_all_guitars = "https://evenstadmusikk.no/search?Filter=PrdGruppeLev2ID%C2%A41:PrdGruppeLev2ID%C2%A41_37%7CPrdGruppeLev1ID%C2%A41:PrdGruppeLev1ID%C2%A41_7&q=lagersalg"
-        url = url_Ibanez_guitars_and_bass
         driver.get(url)
-
-        time.sleep(5)
-
+        time.sleep(2)
         guitars = []
         products = driver.find_elements(By.CLASS_NAME, "WebPubElement.pub-productlisting")
 
