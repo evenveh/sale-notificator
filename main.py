5351b637fead468cd21923324df40badc5ee04bf
evenveh <even.vehus@gmail.com>
2025-03-19 11:48:35 +0100

    Sending custom message from sale_comparator

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import time
 def main_loop():
     test = True
     old_guitars, url = get_guitar_sales()
-
+    old_guitars = ['Ibanez EHB1505-SWL El-gitar: 25%', 'Ibanez GB10SE-BS George Benson: 10%', 'Ibanez S1070PBZ-CLB Elgitar: 10%', 'Ibanez TWP10: 10%']
     while test:
         guitars, url = get_guitar_sales()
         old_guitars = compare_sales(old_products=old_guitars, new_products=guitars, url = url)
-        #send_mail(guitars, url)
         time.sleep(60*60)
 
 
