b75572808e6fc69e32a043c09a8051949c517a4b
evenveh <even.vehus@gmail.com>
2025-03-19 14:44:19 +0100

    Removed test

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 def main_loop():
     test = True
     old_guitars, url = get_guitar_sales()
-    old_guitars = ['Ibanez EHB1505-SWL El-gitar: 25%', 'Ibanez GB10SE-BS George Benson: 10%', 'Ibanez S1070PBZ-CLB Elgitar: 10%', 'Ibanez TWP10: 10%']
     while test:
         guitars, url = get_guitar_sales()
         old_guitars = compare_sales(old_products=old_guitars, new_products=guitars, url = url)
