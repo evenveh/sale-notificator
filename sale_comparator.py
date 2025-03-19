aaa726eb286eb59874693f9d2219f85c127e50cc
evenveh <even.vehus@gmail.com>
2025-03-19 11:30:56 +0100

    creating a string to unwrap products list into string

--- sale_comparator.py
+++ sale_comparator.py
@@ -1,4 +1,4 @@
-from notification import send_mail
+from notification import send_mail, unwrap_product_string
 
 
 def compare_sales(old_products, new_products, url):
@@ -9,11 +9,15 @@ def compare_sales(old_products, new_products, url):
         for product in new_products:
             if product not in old_products:
                 print(f"New product on sale: {product}")
+                products_string = unwrap_product_string(new_products)
+
                 send_mail(new_products, url)
 
         for product in old_products:
             if product not in new_products:
                 print(f"Guitar no longer on sale: {product}")
+                products_string = unwrap_product_string(new_products)
+
                 send_mail(new_products, url)
 
     return new_products
