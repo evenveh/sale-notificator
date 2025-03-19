771ca6e6a6defd3a70c5cd80d15478f2fc54c4c0
evenveh <even.vehus@gmail.com>
2025-03-19 10:27:38 +0100

    Creating a while loop for infinite run

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from notification import send_mail
 from sale_finder import get_guitar_sales
+import time
+
+def main_loop():
+    test = True
+    while test:
+        guitars, url = get_guitar_sales()
+        send_mail(guitars, url)
+        time.sleep(60*60)
 
 
 if __name__ == "__main__":
-    guitars, url = get_guitar_sales()
-    send_mail(guitars, url)
+    main_loop()
