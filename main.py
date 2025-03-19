d90965a6f39f12ccccc1075ddade9b9b7fc925eb
evenveh <even.vehus@gmail.com>
2025-03-19 11:11:04 +0100

    Created functions to compare sales

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from notification import send_mail
 from sale_finder import get_guitar_sales
+from sale_comparator import compare_sales
 import time
 
 def main_loop():
     test = True
+    old_guitars, url = get_guitar_sales()
     while test:
         guitars, url = get_guitar_sales()
+        old_guitars = compare_sales(old_products=old_guitars, new_products=guitars)
         send_mail(guitars, url)
         time.sleep(60*60)
 
