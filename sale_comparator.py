a2d31f5309be26d42736685caff8399f838b60d8
evenveh <even.vehus@gmail.com>
2025-03-20 09:16:51 +0100

    Removed unused variable

--- sale_comparator.py
+++ sale_comparator.py
@@ -7,7 +7,6 @@ def compare_sales(old_products, new_products, url):
     else:
         for product in new_products:
             if product not in old_products:
-                msg = f"New product on sale: {product}"
                 products_string = unwrap_product_string(new_products)
                 message = f"Subject: New Ibanez guitar at Evenstad Outlet\n\nThere is a new product on Evenstad's outlet page: {product}. \n There are currently {len(new_products)} guitars out: \n{products_string}\n\n URL: {url}"
 
@@ -15,7 +14,6 @@ def compare_sales(old_products, new_products, url):
 
         for product in old_products:
             if product not in new_products:
-                msg = f"Guitar no longer on sale: {product}"
                 products_string = unwrap_product_string(new_products)
                 message = f"Subject: Product no longer for sale at Evenstad's Outlet\n\n The product: {product} is no longer for sale. \n There are currently {len(new_products)} guitars out: \n{products_string}\n\n URL: {url}"
 
