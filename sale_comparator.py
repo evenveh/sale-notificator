653dfd5eddd7f65f1b8e87e7357946759b3cc089
evenveh <even.vehus@gmail.com>
2025-03-20 20:56:13 +0100

    Adding space

--- sale_comparator.py
+++ sale_comparator.py
@@ -11,14 +11,14 @@ def compare_sales(old_products, new_products, url):
         for product in new_products:
             if product not in old_products:
                 products_string = unwrap_product_string(new_products)
-                message = f"Subject: New Ibanez guitar at Evenstad Outlet\n\nThere is a new product on Evenstad's outlet page: {product}. \n There are currently {len(new_products)} guitars out: \n{products_string}\n\n URL: {url}"
+                message = f"Subject: New Ibanez guitar at Evenstad Outlet\n\nThere is a new product on Evenstad's outlet page: {product}. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
 
                 send_mail(message)
 
         for product in old_products:
             if product not in new_products:
                 products_string = unwrap_product_string(new_products)
-                message = f"Subject: Product no longer for sale at Evenstad's Outlet\n\n The product: {product} is no longer for sale. \n There are currently {len(new_products)} guitars out: \n{products_string}\n\n URL: {url}"
+                message = f"Subject: Product no longer for sale at Evenstad's Outlet\n\n The product: {product} is no longer for sale. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
 
                 send_mail(message)
 
