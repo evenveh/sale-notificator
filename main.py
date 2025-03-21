e50ba621527a85546585c71e01b12769c4920c5d
evenveh <even.vehus@gmail.com>
2025-03-21 11:43:17 +0100

    Changing url and frequency

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from secrets import URL_ALL_GUITARS, URL_IBANEX_GUITARS, URL_IBANEZ_GUITARS_AND_
 import time
 
 def main_loop():
-    url = URL_ALL_GUITARS
+    url = URL_IBANEZ_GUITARS_AND_BASS
     test = True
     old_guitars = get_sales(url)
     while test:
         guitars = get_sales(url)
         old_guitars = compare_sales(old_products=old_guitars, new_products=guitars, url = url)
-        time.sleep(60*60*6)
+        time.sleep(60*60*3)
 
 if __name__ == "__main__":
     main_loop()
