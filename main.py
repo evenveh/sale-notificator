bac54dfb350c82e3b4a263d106280daf73217003
evenveh <even.vehus@gmail.com>
2025-03-21 11:45:39 +0100

    Whitespace

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main_loop():
     url = URL_IBANEZ_GUITARS_AND_BASS
     test = True
     old_guitars = get_sales(url)
+
     while test:
         guitars = get_sales(url)
         old_guitars = compare_sales(old_products=old_guitars, new_products=guitars, url = url)
