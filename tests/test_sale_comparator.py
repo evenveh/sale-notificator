158f100e59bcfb2e80ad63839dc1f052eeadc51e
evenveh <even.vehus@gmail.com>
2025-03-23 19:59:16 +0100

    testing ci

--- tests/test_sale_comparator.py
+++ tests/test_sale_comparator.py
@@ -3,16 +3,14 @@ from unittest.mock import patch
 
 class TestCompareSales(unittest.TestCase):
 
-    @patch('sale_comparator.send_mail')
-    @patch('sale_comparator.unwrap_product_string')
+
     def test_no_changes_in_sales(self, mock_unwrap, mock_send_mail):
         old_products = ['Product A', 'Product B']
         new_products = ['Product A', 'Product B']
         url = 'http://example.com'
 
 
-    @patch('sale_comparator.send_mail')
-    @patch('sale_comparator.unwrap_product_string')
+
     def test_new_products_on_sale(self, mock_unwrap, mock_send_mail):
         old_products = ['Product A']
         new_products = ['Product A', 'Product B']
@@ -21,8 +19,6 @@ class TestCompareSales(unittest.TestCase):
         mock_unwrap.return_value = 'Product A, Product B'
 
 
-    @patch('sale_comparator.send_mail')
-    @patch('sale_comparator.unwrap_product_string')
     def test_products_no_longer_on_sale(self, mock_unwrap, mock_send_mail):
         old_products = ['Product A', 'Product B']
         new_products = ['Product A']
@@ -31,8 +27,6 @@ class TestCompareSales(unittest.TestCase):
         mock_unwrap.return_value = 'Product A'
 
 
-    @patch('sale_comparator.send_mail')
-    @patch('sale_comparator.unwrap_product_string')
     def test_both_new_and_expired_products(self, mock_unwrap, mock_send_mail):
         old_products = ['Product A']
         new_products = ['Product B']
