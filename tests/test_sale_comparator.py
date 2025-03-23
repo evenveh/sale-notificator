7ca4ddc5fdc69fb695c54a547320231aed5871b3
evenveh <even.vehus@gmail.com>
2025-03-23 20:03:49 +0100

    testing ci

--- tests/test_sale_comparator.py
+++ tests/test_sale_comparator.py
@@ -3,12 +3,10 @@ from unittest.mock import patch
 
 class TestCompareSales(unittest.TestCase):
 
-
     def test_no_changes_in_sales(self):
         old_products = ['Product A', 'Product B']
         new_products = ['Product A', 'Product B']
         url = 'http://example.com'
-
 
 
     def test_new_products_on_sale(self):
@@ -17,20 +15,16 @@ class TestCompareSales(unittest.TestCase):
         url = 'http://example.com'
 
 
-
     def test_products_no_longer_on_sale(self):
         old_products = ['Product A', 'Product B']
         new_products = ['Product A']
         url = 'http://example.com'
 
 
-
-
     def test_both_new_and_expired_products(self):
         old_products = ['Product A']
         new_products = ['Product B']
         url = 'http://example.com'
-
 
 
 if __name__ == '__main__':
