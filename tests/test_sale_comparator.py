b8c68903afc8ba164fd86688b18028ae8341bfa6
evenveh <even.vehus@gmail.com>
2025-03-23 20:00:35 +0100

    testing ci

--- tests/test_sale_comparator.py
+++ tests/test_sale_comparator.py
@@ -4,35 +4,33 @@ from unittest.mock import patch
 class TestCompareSales(unittest.TestCase):
 
 
-    def test_no_changes_in_sales(self, mock_unwrap, mock_send_mail):
+    def test_no_changes_in_sales(self):
         old_products = ['Product A', 'Product B']
         new_products = ['Product A', 'Product B']
         url = 'http://example.com'
 
 
 
-    def test_new_products_on_sale(self, mock_unwrap, mock_send_mail):
+    def test_new_products_on_sale(self):
         old_products = ['Product A']
         new_products = ['Product A', 'Product B']
         url = 'http://example.com'
 
-        mock_unwrap.return_value = 'Product A, Product B'
 
 
-    def test_products_no_longer_on_sale(self, mock_unwrap, mock_send_mail):
+    def test_products_no_longer_on_sale(self):
         old_products = ['Product A', 'Product B']
         new_products = ['Product A']
         url = 'http://example.com'
 
-        mock_unwrap.return_value = 'Product A'
 
 
-    def test_both_new_and_expired_products(self, mock_unwrap, mock_send_mail):
+
+    def test_both_new_and_expired_products(self):
         old_products = ['Product A']
         new_products = ['Product B']
         url = 'http://example.com'
 
-        mock_unwrap.side_effect = ['Product B', 'Product A']
 
 
 if __name__ == '__main__':
