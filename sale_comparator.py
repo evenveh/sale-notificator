11821535e8419efd2c1ca705f8b7ea8a7b41b44e
evenveh <even.vehus@gmail.com>
2025-03-23 11:51:39 +0100

    appending new and old product lists

--- sale_comparator.py
+++ sale_comparator.py
@@ -12,14 +12,14 @@ def compare_sales(old_products, new_products, url):
             if product not in old_products:
                 products_string = unwrap_product_string(new_products)
                 message = f"Subject: New Ibanez guitar at Evenstad Outlet\n\nThere is a new product on Evenstad's outlet page: {product}. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
-
+                new_sales.append(product)
                 send_mail(message)
 
         for product in old_products:
             if product not in new_products:
                 products_string = unwrap_product_string(new_products)
                 message = f"Subject: Product no longer for sale at Evenstad's Outlet\n\n The product: {product} is no longer for sale. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
-
+                expired_sales.append(product)
                 send_mail(message)
 
     return new_products
