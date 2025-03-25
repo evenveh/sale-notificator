39226b837aa60eaa8f55c1d8e36b064155bc2d27
evenveh <even.vehus@gmail.com>
2025-03-25 10:26:59 +0100

    Refactor main loop to retrieve URL secret directly and improve mail notification format

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from notification import send_mail
 from sale_finder import get_sales
 from sale_comparator import compare_sales
-from secrets import URL_IBANEZ_GUITARS_AND_BASS
 import time
 from get_secrets import get_secret
 
 def main_loop():
-    url_secret = get_secret("URL_IBANEZ_GUITARS_AND_BASS")
-    send_mail(message=f"Subject: Sale Notificator\n\nSale notificator is up and running! url:{url_secret}")
-
-    url = URL_IBANEZ_GUITARS_AND_BASS
-    test = True
+    url = get_secret("URL_IBANEZ_GUITARS_AND_BASS")
+    send_mail(message=f"Subject: Sale Notificator\n\nSale notificator is up and running!\n\n url:\n{url}")
     old_guitars = get_sales(url)
 
-    while test:
+    while True:
         guitars = get_sales(url)
         old_guitars = compare_sales(old_products=old_guitars, new_products=guitars, url = url)
         time.sleep(60*60*3)
