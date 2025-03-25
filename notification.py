2cf99993df98d98d48839028debbe4508952e637
evenveh <even.vehus@gmail.com>
2025-03-25 10:41:41 +0100

    Adding try/except

--- notification.py
+++ notification.py
@@ -6,11 +6,15 @@ def unwrap_product_string(products):
     return product_string
 
 def send_mail(message):
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=get_secret("FROM_EMAIL"), password=get_secret("PASSWORD"))
-        connection.sendmail(from_addr=get_secret("FROM_EMAIL"),
-                            to_addrs=get_secret("RECIPIENT"),
-                            msg = message)
+    try:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=get_secret("FROM_EMAIL"), password=get_secret("PASSWORD"))
+            connection.sendmail(from_addr=get_secret("FROM_EMAIL"),
+                                to_addrs=get_secret("RECIPIENT"),
+                                msg = message)
 
-    print(f"An email has been sent to {get_secret("RECIPIENT")}")
+        print(f"An email has been sent to {get_secret("RECIPIENT")}")
+
+    except Exception as e:
+        print(f"Failed sending mail.\n Exception: {e}")
