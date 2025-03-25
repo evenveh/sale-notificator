fd0a2d8ed746d4e7d44d64dfb6381ff3a5f1cecc
evenveh <even.vehus@gmail.com>
2025-03-25 10:34:56 +0100

    Formatting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sale_comparator import compare_sales
 import time
 from get_secrets import get_secret
 
+
 def main_loop():
     url = get_secret("URL_IBANEZ_GUITARS_AND_BASS")
     send_mail(message=f"Subject: Sale Notificator\n\nSale notificator is up and running!\n\n url:\n{url}")
@@ -11,8 +12,9 @@ def main_loop():
 
     while True:
         guitars = get_sales(url)
-        old_guitars = compare_sales(old_products=old_guitars, new_products=guitars, url = url)
-        time.sleep(60*60*3)
+        old_guitars = compare_sales(old_products=old_guitars, new_products=guitars, url=url)
+        time.sleep(60 * 60 * 3)
+
 
 if __name__ == "__main__":
     main_loop()
