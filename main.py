203d9c287ecf45ebdc32c1f1e9f9806be108ed90
evenveh <even.vehus@gmail.com>
2025-03-25 09:50:26 +0100

    Adapting the program to run with secrets

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from sale_finder import get_sales
 from sale_comparator import compare_sales
 from secrets import URL_IBANEZ_GUITARS_AND_BASS
 import time
+from get_secrets import get_secret
 
 def main_loop():
-    send_mail(message=f"Subject: Sale Notificator\n\nSale notificator is up and running!")
+    url_secret = get_secret("URL_IBANEZ_GUITARS_AND_BASS")
+    send_mail(message=f"Subject: Sale Notificator\n\nSale notificator is up and running! url:{url_secret}")
 
     url = URL_IBANEZ_GUITARS_AND_BASS
     test = True
--- notification.py
+++ notification.py
@@ -1,5 +1,6 @@
 import smtplib
 from secrets import PASSWORD, FROM_EMAIL, RECIPIENT
+from get_secrets import get_secret
 
 def unwrap_product_string(products):
     product_string = "\n".join(products)
@@ -8,9 +9,9 @@ def unwrap_product_string(products):
 def send_mail(message):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(user=FROM_EMAIL, password=PASSWORD)
-        connection.sendmail(from_addr=FROM_EMAIL,
-                            to_addrs=RECIPIENT,
+        connection.login(user=get_secret("FROM_EMAIL"), password=get_secret("PASSWORD"))
+        connection.sendmail(from_addr=get_secret("FROM_EMAIL"),
+                            to_addrs=get_secret("RECIPIENT"),
                             msg = message)
 
-    print(f"An email has been sent to {RECIPIENT}")
+    print(f"An email has been sent to {get_secret("RECIPIENT")}")
