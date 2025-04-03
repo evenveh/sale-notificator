54b22ff34687a9dc651f16806f9a31211a6c986d
evenveh <even.vehus@gmail.com>
2025-04-03 17:24:56 +0200

    Creating create_message function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from notification import send_mail, unwrap_product_string
 from sale_finder import get_sales
-from sale_comparator import compare_sales
+from sale_comparator import compare_sales, create_message
 import time
 from get_secrets import get_secret
 
@@ -17,7 +17,8 @@ def initialization():
 def main_loop(old_guitars, url):
     while True:
         guitars = get_sales(url)
-        compare_sales(old_products=old_guitars, new_products=guitars, url=url)
+        new_products, old_products = compare_sales(old_products=old_guitars, new_products=guitars)
+        message = create_message(new_products=new_products, old_products=old_products, url=url)
         old_guitars = guitars
         time.sleep(60 * 60 * 3)
         print("Starting new loop")
--- sale_comparator.py
+++ sale_comparator.py
@@ -1,29 +1,34 @@
 from notification import send_mail, unwrap_product_string
 
-def create_message(new_sales, old_sales, url):
-    if new_sales & old_sales:
+def create_message(new_products, old_products, url):
+    if new_products & old_products:
         msg = "There are new sales and old sales. \n\nNew:blabla \n\nold:albalba"
-    if new_sales and not old_sales:
-        msg = "There are new sales. \n\nNew:blabla"
-    if old_sales and not new_sales:
-        msg = "There are old sales. \n\nold:albalba"
+
+    if new_products and not old_products:
+        products_string = unwrap_product_string(new_products)
+        message = f"Subject: New Ibanez guitar at Evenstad Outlet\n\nThere is a new product on Evenstad's outlet page: {product}. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
+        print("Sending message about new product")
+        send_mail(message)
+
+    if old_products and not new_products:
+        products_string = unwrap_product_string(new_products)
+        message = f"Subject: Product no longer for sale at Evenstad's Outlet\n\n The product: {product} is no longer for sale. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
+        print("Sending message about product no longer for sale")
+        send_mail(message)
 
 
-def compare_sales(old_products, new_products, url):
+def compare_sales(old_products, new_products):
+    new_products = []
+    old_products = []
     if old_products == new_products:
         print("No change in the sales.")
         return new_products
     else:
         for product in new_products:
             if product not in old_products:
-                products_string = unwrap_product_string(new_products)
-                message = f"Subject: New Ibanez guitar at Evenstad Outlet\n\nThere is a new product on Evenstad's outlet page: {product}. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
-                print("Sending message about new product")
-                send_mail(message)
-
+                new_products.append(product)
         for product in old_products:
             if product not in new_products:
-                products_string = unwrap_product_string(new_products)
-                message = f"Subject: Product no longer for sale at Evenstad's Outlet\n\n The product: {product} is no longer for sale. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
-                print("Sending message about product no longer for sale")
-                send_mail(message)
+                old_products.append(product)
+
+    return new_products, old_products
