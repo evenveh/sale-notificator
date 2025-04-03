a7438f78a876bef936c16165bead377c2244d1bf
evenveh <even.vehus@gmail.com>
2025-04-03 18:19:02 +0200

    Refactor create_message function to improve message formatting and handling of product updates

--- sale_comparator.py
+++ sale_comparator.py
@@ -2,20 +2,25 @@ from notification import send_mail, unwrap_product_string
 
 def create_message(new_products, old_products, url):
     if new_products & old_products:
-        message = f"Subject: Update on sales\n\n New products.... Expired products...URL: {url}"
-        print("Sending message about product no longer for sale")
+        new_products_string = unwrap_product_string(new_products)
+        old_products_string = unwrap_product_string(old_products)
+        message = f"Subject: Update on sales\n\n New products: {new_products_string}\n\n Expired products:{old_products_string} \n\nURL: {url}"
+        send_mail(message)
+        print("Sending message about both new and expired products")
 
     if new_products and not old_products:
-        products_string = unwrap_product_string(new_products)
-        message = f"Subject: New Ibanez guitar at Evenstad Outlet\n\nThere is a new product on Evenstad's outlet page: {product}. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
+        new_products_string = unwrap_product_string(new_products)
+        message = f"Subject: New Ibanez guitar at Evenstad Outlet\n\nThere is a new product on Evenstad's outlet page: {new_products_string }. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
         print("Sending message about new product")
         send_mail(message)
 
     if old_products and not new_products:
-        products_string = unwrap_product_string(new_products)
-        message = f"Subject: Product no longer for sale at Evenstad's Outlet\n\n The product: {product} is no longer for sale. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
+        old_products_string = unwrap_product_string(new_products)
+        message = f"Subject: Product no longer for sale at Evenstad's Outlet\n\n The product: {old_products_string } is no longer for sale. \n There are currently {len(new_products)} guitars out: \n\n{products_string}\n\n URL: {url}"
         print("Sending message about product no longer for sale")
         send_mail(message)
+    else:
+        return
 
 
 def compare_sales(old_products, new_products):
