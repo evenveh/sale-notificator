cbb7e306e294a26dcf520bdde8855275246f8080
evenveh <even.vehus@gmail.com>
2025-04-11 12:57:37 +0200

    Formatting

--- tests/test_sale_comparator.py
+++ tests/test_sale_comparator.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 from sale_comparator import compare_sales, create_message
 
+
 class TestCompareSales(unittest.TestCase):
 
     @patch('sale_comparator.send_mail')
@@ -39,7 +40,6 @@ class TestCompareSales(unittest.TestCase):
         )
         mock_send_mail.assert_called_once_with(message=expected_message)
 
-
     @patch('sale_comparator.send_mail')
     @patch('sale_comparator.unwrap_product_string')
     def test_create_message_with_only_expired_sales(self, mock_unwrap, mock_send_mail):
@@ -75,7 +75,7 @@ class TestCompareSales(unittest.TestCase):
         old_sales = ['Product A', 'Product B']
         current_sales = ['Product A', 'Product B']
 
-        new_products, expired_products = compare_sales(old_products=old_sales, current_products = current_sales)
+        new_products, expired_products = compare_sales(old_products=old_sales, current_products=current_sales)
         self.assertEqual(new_products, [])
         self.assertEqual(expired_products, [])
 
@@ -83,27 +83,23 @@ class TestCompareSales(unittest.TestCase):
     def test_new_products_on_sale(self, mock_unwrap):
         old_sales = ['Product A']
         current_sales = ['Product A', 'Product B']
-        new_products, expired_products = compare_sales(old_products = old_sales, current_products= current_sales)
+        new_products, expired_products = compare_sales(old_products=old_sales, current_products=current_sales)
         self.assertEqual(new_products, ['Product B'])
         self.assertEqual(expired_products, [])
-
 
     @patch('sale_comparator.unwrap_product_string')
     def test_products_no_longer_on_sale(self, mock_unwrap):
         old_sales = ['Product A', 'Product B']
         current_sales = ['Product A']
-        new_products, expired_products = compare_sales(old_products=old_sales, current_products= current_sales)
+        new_products, expired_products = compare_sales(old_products=old_sales, current_products=current_sales)
         self.assertEqual(new_products, [])
         self.assertEqual(expired_products, ['Product B'])
-
-
-
 
     @patch('sale_comparator.unwrap_product_string')
     def test_both_new_and_expired_products(self, mock_unwrap):
         old_sales = ['Product A']
         current_sales = ['Product B']
-        new_products, expired_products = compare_sales(old_products=old_sales, current_products= current_sales)
+        new_products, expired_products = compare_sales(old_products=old_sales, current_products=current_sales)
         self.assertEqual(new_products, ['Product B'])
         self.assertEqual(expired_products, ['Product A'])
 
