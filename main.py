07dd288cd998bbad7f18c97a5e69a8827823a2da
evenveh <even.vehus@gmail.com>
2025-05-14 14:40:49 +0200

    Refactor initialization to accept URL as input and update product retrieval logic

--- main.py
+++ main.py
@@ -5,28 +5,33 @@ import time
 from get_secrets import get_secret
 
 
-def initialization(page_scraper):
-    url = get_secret("URL_IBANEZ_GUITARS_AND_BASS")
-    initial_sales = page_scraper.find_products_on_page(url)
-    products_string = unwrap_product_string(initial_sales)
+def initialization(page_scraper, url):
+    initial_products_on_outlet = page_scraper.find_products_on_page(url)
+    products_string = unwrap_product_string(initial_products_on_outlet)
     send_mail(message=f"Subject: Sale Notificator\n\nSale notificator is up and running!\n\n "
-                      f"There are currently {len(initial_sales)} guitars out: \n\n{products_string}\n\n URL: {url}")
-    return initial_sales, url
+                      f"There are currently {len(initial_products_on_outlet)} guitars out: "
+                      f"\n\n{products_string}\n\n URL: {url}")
+    return initial_products_on_outlet
 
 
 def main_loop(old_sales, url, page_scraper):
     while True:
-        current_sales = page_scraper.find_products_on_page(url)
-        new_sales, expired_sales = compare_sales(old_products=old_sales, current_products=current_sales)
-        message = create_message(new_sales=new_sales, expired_sales=expired_sales, current_sales=current_sales, url=url)
+        current_products_on_outlet = page_scraper.find_products_on_page(url)
+        new_products_on_outlet, expired_products_on_outlet = compare_sales(old_products=old_sales,
+                                                                           current_products=current_products_on_outlet)
+        message = create_message(new_sales=new_products_on_outlet,
+                                 expired_sales=expired_products_on_outlet,
+                                 current_sales=current_products_on_outlet,
+                                 url=url)
         send_mail(message)
-        old_sales = current_sales
+        old_sales = current_products_on_outlet
 
         time.sleep(60 * 60 * 3)
         print("Starting new loop")
 
 
 if __name__ == "__main__":
+    url = get_secret("URL_IBANEZ_GUITARS_AND_BASS")
     page_scraper = PageScraper()
-    initial_sales, url = initialization(page_scraper)
+    initial_sales = initialization(page_scraper, url)
     main_loop(initial_sales, url, page_scraper)
