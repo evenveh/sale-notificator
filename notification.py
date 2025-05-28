bdefd79aec0816f4fa03b793569c38bdcdd7c2d0
evenveh <even.vehus@gmail.com>
2025-05-28 16:24:50 +0200

    Creating functionality to send custom mails to the recipients that subscribes to the products

--- notification.py
+++ notification.py
@@ -7,8 +7,21 @@ def unwrap_product_string(products):
     return product_string
 
 
-def craft_message_for_updated_prices(price_dict):
-    price_lines = [f"{key}: {value['price']}" for key, value in price_dict.items()]
+def sort_recipients_and_subscriptions(price_dict):
+    """ Sorts and returns a list of unique subscribers from the price dictionary.
+    :param price_dict: A dictionary containing products and related information, including subscribers.
+    :return: a list of unique subscribers from the price_dict
+    """
+    subscribers = []
+    for product, details in price_dict.items():
+        for subscriber in details.get("subscribers", []):
+            subscribers.append(subscriber)
+    return list(set(subscribers))
+
+
+def craft_message_for_updated_prices(price_dict, subscriber):
+    price_lines = [f"{key}: {value['price']}" for key, value in price_dict.items() if
+                   subscriber in value["subscribers"]]
 
     msg = "Subject: Price update\n\nHere are the prices:\n\n" + "\n".join(price_lines)
 
