ce4a6d707010a37e2a160bd6515f906ff88a1f84
evenveh <even.vehus@gmail.com>
2025-05-28 10:52:11 +0200

    Adding subscribers to the config file

--- configuration_file.py
+++ configuration_file.py
@@ -4,29 +4,35 @@
 # 2. price_tag - The CSS selector to find the price on the product page
 # 3. price - The current price of the product, initialized to 1.0, will be a dynamic value fetched from the website.
 
+
 price_dict = {"Fiocchi 9mm 115 grs": {
     "url": "https://www.xxl.no/fiocchi-fiocchi-9mm-115-grs-fmj-ammunisjon/p/9000961_2_Style",
     "price_tag": '[data-testid="selling-price"]',
+    "subscribers": ["workaholix55@gmail.com"],
     "price": 1.0
-    },
+},
     "Fiocchi 9mm 124 grs": {
         "url": "https://www.xxl.no/fiocchi-cart-9-luger-fmj-124-ram-ammunisjon/p/1185862_1_Style",
         "price_tag": '[data-testid="selling-price"]',
+        "subscribers": ["workaholix55@gmail.com"],
         "price": 1.0
     },
     "CCI 22lr": {
         "url": "https://www.xxl.no/cci-cci-standard-22-lr-40gr-lrn-gun-rifle-ammunisjon/p/1189986_1_Style",
         "price_tag": '[data-testid="selling-price"]',
+        "subscribers": ["workaholix55@gmail.com"],
         "price": 1.0
     },
     "30-06": {
         "url": "https://www.xxl.no/fiocchi-30-06-springfield-147-fmj-ammunisjon/p/1147062_1_Style",
         "price_tag": '[data-testid="selling-price"]',
+        "subscribers": ["workaholix55@gmail.com"],
         "price": 1.0
     },
     "Fiocchi Steel 12/70": {
         "url": "https://www.xxl.no/fiocchi-fsteel-24g-hagleammunisjon/p/1224197_1_Style",
         "price_tag": '[data-testid="selling-price"]',
+        "subscribers": ["workaholix55@gmail.com"],
         "price": 1.0
     }
 }
@@ -37,6 +43,7 @@ product_page_dict = {"Evenstad outlet": {
     "product_class_name": "WebPubElement.pub-productlisting",
     "product_name_tag": "AddHeader1",
     "product_sale_tag": "YouSavePercentLabel",
+    "subscribers": ["workaholix55@gmail.com"],
     "products_on_page": []  # Could be a dict also
-    }
+}
 }
