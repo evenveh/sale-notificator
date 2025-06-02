08925f3bcb745a58bce47a7ad0ddd9753e8463b5
evenveh <even.vehus@gmail.com>
2025-06-02 15:57:47 +0200

    Adding mail adresses to list and a threshold value

--- configuration_file.py
+++ configuration_file.py
@@ -9,32 +9,37 @@ price_dict = {
     "Fiocchi 9mm 115 grs": {
         "url": "https://www.xxl.no/fiocchi-fiocchi-9mm-115-grs-fmj-ammunisjon/p/9000961_2_Style",
         "price_tag": '[data-testid="selling-price"]',
-        "subscribers": ["workaholix55@gmail.com", "even.vehus@gmail.com"],
-        "price": 1.0
+        "subscribers": ["workaholix55@gmail.com", "even.vehus@gmail.com", "robin.frohaug@gmail.com"],
+        "price": 1.0,
+        "threshold": 155.0
     },
     "Fiocchi 9mm 124 grs": {
         "url": "https://www.xxl.no/fiocchi-cart-9-luger-fmj-124-ram-ammunisjon/p/1185862_1_Style",
         "price_tag": '[data-testid="selling-price"]',
-        "subscribers": ["workaholix55@gmail.com", "even.vehus@gmail.com"],
-        "price": 1.0
+        "subscribers": ["workaholix55@gmail.com","robin.frohaug@gmail.com"],
+        "price": 1.0,
+        "threshold": 155.0
     },
     "CCI 22lr": {
         "url": "https://www.xxl.no/cci-cci-standard-22-lr-40gr-lrn-gun-rifle-ammunisjon/p/1189986_1_Style",
         "price_tag": '[data-testid="selling-price"]',
         "subscribers": ["workaholix55@gmail.com"],
-        "price": 1.0
+        "price": 1.0,
+        "threshold": 55
     },
     "30-06": {
         "url": "https://www.xxl.no/fiocchi-30-06-springfield-147-fmj-ammunisjon/p/1147062_1_Style",
         "price_tag": '[data-testid="selling-price"]',
         "subscribers": ["workaholix55@gmail.com"],
-        "price": 1.0
+        "price": 1.0,
+        "threshold": 200.0
     },
     "Fiocchi Steel 12/70": {
         "url": "https://www.xxl.no/fiocchi-fsteel-24g-hagleammunisjon/p/1224197_1_Style",
         "price_tag": '[data-testid="selling-price"]',
         "subscribers": ["workaholix55@gmail.com"],
-        "price": 1.0
+        "price": 1.0,
+        "threshold": 75.0
     }
 }
 
