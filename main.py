b746273df3261ec3ca600458579808add33bbc72
evenveh <even.vehus@gmail.com>
2025-06-10 17:37:38 +0200

    CHanging main to not run in a loop. If the checks pass, a mail will be sent to the subscribers.
    At a later point, the csv can be stored again, then the prices can be compared

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from notification import find_all_subscribers, send_mail_to_subscibers
 from sale_finder import PageScraper
-from time import sleep
 from configuration_file import load_csv_to_price_dict
 
 
-def main_loop(page_scraper, price_dict):
-    while True:
-        price_dict = page_scraper.update_all_prices(price_dict)
-        subscribers = find_all_subscribers(price_dict)
-        send_mail_to_subscibers(subscribers, price_dict)
-
-        sleep(60 * 60 * 12)
+def main(page_scraper, price_dict):
+    price_dict = page_scraper.update_all_prices(price_dict)
+    subscribers = find_all_subscribers(price_dict)
+    send_mail_to_subscibers(subscribers, price_dict)
 
 
 if __name__ == "__main__":
     price_dict = load_csv_to_price_dict("product_overview.csv")
     page_scraper = PageScraper()
-    main_loop(page_scraper, price_dict)
+    main(page_scraper, price_dict)
