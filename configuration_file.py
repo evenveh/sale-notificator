2cc70ea13e62ccc20c0835457eb9dc5081054b72
evenveh <even.vehus@gmail.com>
2025-06-16 08:58:50 +0200

    Namings and typos

--- configuration_file.py
+++ configuration_file.py
@@ -74,7 +74,7 @@ product_page_dict = {
 
 
 
-#_________Some utility functions to convet the price dictionary as csv, and convert a csv to a price dictionary_________
+#_________Some utility functions to convert the price dictionary as csv, and convert a csv to a price dictionary_________
 def load_csv_to_price_dict(filename="price_dict.csv"):
     """
     Converts a CSV file back into a dictionary identical to the original price_dict.
@@ -82,18 +82,18 @@ def load_csv_to_price_dict(filename="price_dict.csv"):
     :param filename: Name of the CSV file to read the data from.
     :return: A dictionary containing product details.
     """
-    price_dict2 = {}
+    price_dict = {}
     with open(filename, mode="r", newline="", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            price_dict2[row["Product Name"]] = {
+            price_dict[row["Product Name"]] = {
                 "url": row["URL"],
                 "price_tag": row["Price Tag"],
                 "subscribers": row["Subscribers"].split(", "),
                 "price": float(row["Price"]),
                 "threshold": float(row["Threshold"])
             }
-    return price_dict2
+    return price_dict
 
 
 def save_price_dict_to_csv(price_dict, filename="price_dict.csv"):
